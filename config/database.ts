4cdb322558d51f3fcda3ac9808c8dc3416bd4806
Rpayet <robinpayet@gmail.com>
2024-08-01 13:28:37 +0200

    Update database configuration and user table migration

--- config/database.ts
+++ config/database.ts
@@ -7,11 +7,11 @@ const dbConfig = defineConfig({
     mysql: {
       client: 'mysql2',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        host: env.get('DB_HOST', '127.0.0.1'),
+        port: env.get('DB_PORT', ''),
+        user: env.get('DB_USER', 'root'),
+        password: env.get('DB_PASSWORD', ''),
+        database: env.get('DB_DATABASE', 'firststep'),
       },
       migrations: {
         naturalSort: true,
